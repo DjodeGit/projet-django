68dae3ef84dc1c77d8124f6cd3e10283e5468cf4
DjodeGit <medonjiojodeanas@gmail.com>
2026-02-20 23:14:41 +0100

    modification du fichier settings.py

--- mon_projet/settings.py
+++ mon_projet/settings.py
@@ -98,13 +98,20 @@ import dj_database_url
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASES = {
-    "default": dj_database_url.parse(
-        DATABASE_URL,
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 #if DATABASE_URL:
     # Production (Render → PostgreSQL)
 #    DATABASES = {
